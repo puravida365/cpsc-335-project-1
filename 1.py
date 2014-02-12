d79d3bf98d4f93bc3039781f0627b039b97cf4aa
Josue Mora <josue@inno-live.com>
2014-02-12 00:14:27 -0800

    replaced for loop with max() function

--- 1.py
+++ 1.py
@@ -5,10 +5,5 @@
 # define variables
 str = 'here is a long string, which values is the greatest?'
 
-
 # go through string and find highest ascci value
-for x in str:
-	tmp = ord(x)
-	max = tmp
-	if max < tmp:
-		max = temp;
+print "Max character: " + max(str)
