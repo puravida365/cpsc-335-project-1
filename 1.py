459ebdb2dc888c09f6a541fd253a5eabe9945a10
Josue Mora <josue@inno-live.com>
2014-02-11 19:38:11 -0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+# CPSC 334
+# PROJECT 1
+# QUESTION 1
+
+# define variables
+str = 'here is a long string, which values is the greatest?'
+
+
+# go through string and find highest ascci value
+for x in str:
+	tmp = ord(x)
+	max = tmp
+	if max < tmp:
+		max = temp;
