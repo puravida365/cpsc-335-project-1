cf0557a5584c0dcc62fbb130379425a2e6d51896
Josue Mora <josue@inno-live.com>
2014-02-18 21:02:10 -0800

    max function implementation by iteration

--- 1.py
+++ 1.py
@@ -1,9 +1,48 @@
-# CPSC 334
-# PROJECT 1
-# QUESTION 1
+# ===============================================================
+# CPSC 335 - Spring 2014
+# PROJECT 1 - QUESTION 1
 
-# define variables
+# ===============================================================
+# Given:
+# 
+# Input: a string s of length n > 0
+# output: the character c in s with the greatest integer value
+# size: n
+# ===============================================================
+
+
+# ================ Design Algorithm =============================
+
+
+
+# =============Describe algorithm using pseudocode ==============
+
+
+# ================ mathematical analysis ========================
+
+
+# ================ python implementation ========================
+
+# define variable
+
 str = 'here is a long string, which values is the greatest?'
+max = str[0]
+# loop through all string characters
+for x in str:
+	if x > max:
+		max = x
 
-# go through string and find highest ascci value
-print "Max character: " + max(str)
+print max
+
+
+
+
+
+# ================= performance measurement ======================
+
+
+
+
+# === compare results w/ the efficiency class of the algorithm ===
+
+
